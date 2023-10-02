022e9f11e9e637204c326f3f4c87e31b4cb924e0
Syeda Hafsa <s.hafsa.5124@gmail.com>
2023-10-02 12:17:46 +0400

    Practice 5

---
+++ Chapter 1- Getting Started with Python/Practice/Practice 5.py
@@ -0,0 +1,10 @@
+''' Practice 5
+Write four strings in different variables and print the output as one string 
+(String Concatenation)'''
+
+a= "My "
+b= "name "
+c= "is "
+d= "Hafsa."
+
+print (a + b + c + d)
