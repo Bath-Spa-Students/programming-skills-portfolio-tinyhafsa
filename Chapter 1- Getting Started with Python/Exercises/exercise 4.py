6685950768865001ac48765a55b70a82fb7eccd9
Syeda Hafsa <s.hafsa.5124@gmail.com>
2023-10-02 12:44:39 +0400

    exercise 4

---
+++ Chapter 1- Getting Started with Python/Exercises/exercise 4.py
@@ -0,0 +1,8 @@
+'''Exercise 4: Strings Concatination :ballot_box_with_check:
+Write three strings in different variables and print the output as one string.'''
+
+value_1 = "Road "
+value_2 = "of "
+value_3 = "life."
+
+print (value_1 + value_2 + value_3)
