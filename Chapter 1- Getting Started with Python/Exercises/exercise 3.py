e01c695b962d927d0c8786050d6896997f04f2f6
Syeda Hafsa <s.hafsa.5124@gmail.com>
2023-10-02 12:35:53 +0400

    exercise 3.py

---
+++ Chapter 1- Getting Started with Python/Exercises/exercise 3.py
@@ -0,0 +1,7 @@
+'''Exercise 3: Print date and Time :ballot_box_with_check:
+Write a Python program to display the current date and time.'''
+
+from datetime import datetime
+now = datetime.now()
+
+print ("Current date and time: ", now)
