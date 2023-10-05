bd5d89757edeecad186bfb9d9b43fd0d2f70348b
Syeda Hafsa <s.hafsa.5124@gmail.com>
2023-10-05 11:58:55 +0400

    exercise 2.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise 2.py
@@ -0,0 +1,9 @@
+'''Exercise 2: Variables :ballot_box_with_check:
+Find a quote from a famous person you admire. Print the quote and the name of its author. 
+Your output should look something like the following, including the quotation marks:
+Albert Einstein once said, “A person who never made a mistake never tried anything new.'''
+
+author = "Jane Austen"
+quote = '"Is not general incivility the very essence of love?"'
+
+print (author , "once wrote," , quote)
