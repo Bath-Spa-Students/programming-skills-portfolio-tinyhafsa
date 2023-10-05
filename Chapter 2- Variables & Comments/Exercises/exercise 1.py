7667c86f06efbce2f565a06b3b274cde63148cc2
Syeda Hafsa <s.hafsa.5124@gmail.com>
2023-10-05 11:58:01 +0400

    exercise 1.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise 1.py
@@ -0,0 +1,8 @@
+'''Exercise 1: Variables
+Assign a message to a variable, and print that message.
+Then change the value of the variable to a new message, and print the new message. '''
+
+message = "Welcome back."
+message = "Welcome Home!!" 
+
+print (message)
