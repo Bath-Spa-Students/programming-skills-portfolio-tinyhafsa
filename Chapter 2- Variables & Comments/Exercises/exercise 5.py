0d5006c1f6aa8c88708fcf3e3b383c9ab7c90531
Syeda Hafsa <s.hafsa.5124@gmail.com>
2023-10-12 11:40:37 +0400

    Create exercise 5.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise 5.py
@@ -0,0 +1,10 @@
+## Exercise 5: USB Shopper
+
+total_money = 50
+each_usb = 6
+
+number_of_usb = (total_money//each_usb)
+money_left = (total_money%each_usb)
+
+print (f"She can buy {number_of_usb} USB sticks")
+print (f"She will have {money_left} pounds left.")
