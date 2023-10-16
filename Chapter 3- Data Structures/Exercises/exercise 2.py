4e7a08a6115d43906304e1dd065b84bee8324cf6
Syeda Hafsa <s.hafsa.5124@gmail.com>
2023-10-16 12:38:49 +0400

    Create exercise 2.py

---
+++ Chapter 3- Data Structures/Exercises/exercise 2.py
@@ -0,0 +1,14 @@
+#Exercise 2: Greetings
+
+#variables
+names = ("Khadeeja" , "Alishba" , "Amna" , "Rabiya")
+
+#printed list statements with f'string
+#with first item in list
+print (f"Long time no see {names[0]}!")
+#with second item in list
+print (f"Long time no see {names[1]}!")
+#with third item in list
+print (f"Long time no see {names[2]}!")
+#with fourth item in list
+print (f"Long time no see {names[3]}!")
