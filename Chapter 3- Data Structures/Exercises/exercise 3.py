20d243cef062c8ae072e9a43ee1ba92862b38288
Syeda Hafsa <s.hafsa.5124@gmail.com>
2023-10-16 12:38:53 +0400

    Create exercise 3.py

---
+++ Chapter 3- Data Structures/Exercises/exercise 3.py
@@ -0,0 +1,13 @@
+#Exercise 3: Your Own List 
+
+#list of favourite car companies
+company= "Honda car" , "Mercedes bike" , "Toyota minivan" , "Tesla car"
+
+#first print statement of first item in list
+print (f"I don't like the {company[0]}.")
+#second print statement of second item in list
+print (f"The {company[1]} is expensive.")
+#third print statement of third item in list
+print (f"I own a {company[2]}.")
+#fourth print statement of fourth item in list
+print (f"I want to buy a {company[3]}.")
