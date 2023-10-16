7ff6a232eabefd694d49c97d3a4c6a01590caac2
Syeda Hafsa <s.hafsa.5124@gmail.com>
2023-10-16 12:38:45 +0400

    Create exercise 1.py

---
+++ Chapter 3- Data Structures/Exercises/exercise 1.py
@@ -0,0 +1,13 @@
+#Exercise 1: Names 
+
+#list of names stored in a variable
+names = ["Khadeeja" , "Alishba" , "Amna" , "Rabiya"]
+
+#first printed name
+print (names[0])
+#second printed name
+print (names[1])
+#third printed name
+print (names[2])
+#fourth printed name
+print (names[3])
