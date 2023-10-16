3d3985e4dcc834d79d5db04fb5087ada3d1fb2fc
Syeda Hafsa <s.hafsa.5124@gmail.com>
2023-10-16 12:39:01 +0400

    Create exercise 5.py

---
+++ Chapter 3- Data Structures/Exercises/exercise 5.py
@@ -0,0 +1,16 @@
+## Exercise 5: Change Guest List
+
+#people i would invite to dinner
+guests = ["Alishba", "Amna", "Rabiya"]
+
+#name of frined who won't be able to attend dinner
+print (f"{guests [2]} will not be able to attend")
+#replace the name of friend who can't attend dinner
+guests [2] = "Khadeeja"
+
+#invitation message 
+print (f"Hey, {guests [0]}. Do you want to get together for dinner?")
+#invitation message
+print (f"Hey, {guests [1]}. {guests [0]} and I are getting together for dinner. Do you want to join us?")
+#new invitation message
+print (f"Hey, {guests [2]}. It's been a while. Do you want to catch up over dinner?")
