e20fa818595fa3405d0ef5c8fec2f6d7eec3e140
Syeda Hafsa <s.hafsa.5124@gmail.com>
2023-10-22 16:23:14 +0400

    Update exercise 4.py

--- Chapter 3- Data Structures/Exercises/exercise 4.py
+++ Chapter 3- Data Structures/Exercises/exercise 4.py
@@ -1,12 +1,14 @@
 # Exercise 4: Guest List
 
 #people i would invite to dinner
-guests = ["Alishba", "Amna", "Rabiya"]
+guests = ["Alishba", "Amna", "Rabiya", "Mariam"]
 
 #print message to each person
 #first message
 print (f"Hey, {guests [0]}. Do you want to get dinner together?")
 #second message
-print (f"Hey, {guests [1]}. Let's get dinner at that Chinese place you like.")
+print (f"Hey, {guests [1]}. Let's have dinner at my place and watch a movie together.")
 #third message
-print (f"Hey, {guests [0]}. It's been a while. Do you catch up over dinner?")
+print (f"Hey, {guests [2]}. It's been a while. Do you catch up over dinner?")
+#fourth message
+print (f"Hey, {guests [3]}. I'm inviting you to dinner at my place.")
