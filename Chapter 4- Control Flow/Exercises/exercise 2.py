bb05efc3b8723180260c0ea88d84fa76ae29c7a9
Syeda Hafsa <s.hafsa.5124@gmail.com>
2023-10-28 17:06:28 +0400

    Create exercise 2.py

---
+++ Chapter 4- Control Flow/Exercises/exercise 2.py
@@ -0,0 +1,19 @@
+#Exercise 2: Alien Colors #2 
+
+#version 1: to run if block
+#declaring variable
+alien_color = "green"
+#if-else chain statement
+if alien_color == "green" :
+    print ("You have shot an alien. You have earned 5 points!")
+else:
+    print ("You have earned 10 points!")
+
+#version 2: to run if block
+#declaring variable
+alien_color = "yellow"
+#if-else chain statement
+if alien_color == "green" :
+    print ("You have shot an alien. You have earned 5 points!")
+else:
+    print ("You have earned 10 points!")
