d366e0972b758e588ccbf39c5ccff7f6da9aff55
Syeda Hafsa <s.hafsa.5124@gmail.com>
2023-10-28 17:06:32 +0400

    Create exercise 3.py

---
+++ Chapter 4- Control Flow/Exercises/exercise 3.py
@@ -0,0 +1,34 @@
+#Exercise 3: Alien Colors #3
+
+#version 1
+#declaring variable
+alien_color = "green"
+#if-elifelse chain
+if alien_color == "green" :
+    print ("Player has earned 5 points!")
+elif alien_color == "yellow" :
+    print ("Player has earned 10 points!")
+else:
+    print ("Player has earned 15 points!")
+
+#version 2
+#declaring variable
+alien_color = "yellow"
+#if-elifelse chain
+if alien_color == "green" :
+    print ("Player has earned 5 points!")
+elif alien_color == "yellow" :
+    print ("Player has earned 10 points!")
+else:
+    print ("Player has earned 15 points!")
+
+#version 3
+#declaring variable
+alien_color = "red"
+#if-elifelse chain
+if alien_color == "green" :
+    print ("Player has earned 5 points!")
+elif alien_color == "yellow" :
+    print ("Player has earned 10 points!")
+else:
+    print ("Player has earned 15 points!")
