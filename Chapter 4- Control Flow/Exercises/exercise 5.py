7db5a24f9853c09b14f2e76aeff7b736f2713186
Syeda Hafsa <s.hafsa.5124@gmail.com>
2023-10-28 17:06:39 +0400

    Create exercise 5.py

---
+++ Chapter 4- Control Flow/Exercises/exercise 5.py
@@ -0,0 +1,20 @@
+#Exercise 5: Favorite Fruit
+
+#making list of favorite fruits
+favorite_fruits = ["pomegranates" , "cherries" , "strawberries"]
+
+#if 'apples' is present in list
+if "apples" in favorite_fruits :
+    print ("An apple a day, keeps the doctor away!")
+#if 'bananas' is present in list
+if "bananas" in favorite_fruits :
+    print ("You really like bananas!")
+#if 'cherries' is present in list
+if "cherries" in favorite_fruits :
+    print ("Cherries are very delicious!")
+#if 'grapes' is present in list
+if "grapes" in favorite_fruits :
+    print ("Sour grapes are the best!")
+#if 'strawberries' is present in list
+if "strawberries" in favorite_fruits :
+    print ("I like strawberries too!")
