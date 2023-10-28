eefd55e32cfbebbcbe24a9cdb8444b977693fb0d
Syeda Hafsa <s.hafsa.5124@gmail.com>
2023-10-28 17:06:24 +0400

    Create exercise 1.py

---
+++ Chapter 4- Control Flow/Exercises/exercise 1.py
@@ -0,0 +1,17 @@
+#Exercise 1:  Alien Colors #1
+
+#version 1: passes the if test
+#declaring variable
+alien_color= "green"
+
+#if statement
+if alien_color == "green" :
+    print ("Congratulations, you have earned 5 points.")
+
+#version 2: fails if test - no output
+#declaring variable
+alien_color= "yellow"
+
+#if statement
+if alien_color == "green" :
+    print ("Congratulations, you have earned 5 points.")
