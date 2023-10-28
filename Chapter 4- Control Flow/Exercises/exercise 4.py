4f7d4983f4914fef907b527c7f17dba36b52a12c
Syeda Hafsa <s.hafsa.5124@gmail.com>
2023-10-28 17:06:35 +0400

    Create exercise 4.py

---
+++ Chapter 4- Control Flow/Exercises/exercise 4.py
@@ -0,0 +1,23 @@
+#Exercise 4: Stages of Life 
+
+#declaring variable
+age = 28
+
+#if age is less than 2
+if age < 2 :
+    print ("The person is a baby.")
+#if age is at least 2, but less than 4
+elif age >= 2 and age < 4 : 
+    print ("The person is a toddler.")
+#if age is at least 4, but less than 13
+elif age >= 4 and age < 13 : 
+    print ("The person is a kid.")
+#if age is at least 13, but less than 20
+elif age >= 13 and age < 20 : 
+    print ("The person is a teenager.")
+#if age is at least 20, but less than 65
+elif age >= 20 and age < 65 : 
+    print ("The person is a adult.")
+#if age is 65 or older
+else : 
+    print ("The person is a elder.")
