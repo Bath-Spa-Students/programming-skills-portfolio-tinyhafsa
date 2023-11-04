e9b14d014bb4c014723768c1de0903b044b58e35
Syeda Hafsa <s.hafsa.5124@gmail.com>
2023-11-04 16:03:22 +0400

    Update exercise 1.py

--- Chapter 5- Dictionaries/Exercises/exercise 1.py
+++ Chapter 5- Dictionaries/Exercises/exercise 1.py
@@ -5,10 +5,15 @@ You should have keys such as first_name, last_name, age, and city.
 Print each piece of information stored in your dictionary.'''
 
 #storing keys and values in a dictionary
-dictionary = {"First name" : "Alishba" ,
+dictionary= {"First name" : "Alishba" ,
               "Last name" : "Imtiaz" , 
               "Age" : "18" ,
               "City" : "Sharjah"}
 
 #displaying the dictionary
+print(dictionary["First name"])
+print(dictionary["Last name"])
+print(dictionary["Age"])
+print(dictionary["City"])
+
 print (dictionary)
