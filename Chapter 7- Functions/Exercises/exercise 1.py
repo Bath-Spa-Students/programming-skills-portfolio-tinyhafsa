cc9ad1934f94fa2206cd9660296c632cdc73ca08
Syeda Hafsa <s.hafsa.5124@gmail.com>
2023-11-13 18:52:06 +0400

    Create exercise 1.py

---
+++ Chapter 7- Functions/Exercises/exercise 1.py
@@ -0,0 +1,8 @@
+# Exercise 1:   Message  
+
+#definind a funstion that prints a message
+def display_message():
+    #message that is printed when function is called
+    print ("In this chapter, I will learn about functions in python.")
+#calling the function
+display_message()
