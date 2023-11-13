ffd61c00e3707afec6279989c445368a0f2bb1cb
Syeda Hafsa <s.hafsa.5124@gmail.com>
2023-11-13 17:35:26 +0400

    Create exercise 3.py

---
+++ Chapter 6- Loops/Exercises/exercise 3.py
@@ -0,0 +1,10 @@
+'''## Exercise 3: Infinity :ballot_box_with_check:
+
+Write a loop that never ends, and run it. 
+(To end the loop, press ctrl-C or close the window displaying the output.)'''
+
+#establishing a while loop
+while True:
+    #print statement that will run infinitely
+    print ("A loop that runs forever!")
+    #the loop will run forever since so break statement has been added
