93209827102a4435a061e7f51940228d1836f5f9
Syeda Hafsa <s.hafsa.5124@gmail.com>
2023-11-16 19:17:07 +0400

    Chapter 7 - ex 2, 3 (updates)

--- Chapter 7- Functions/Exercises/exercise 2.py
+++ Chapter 7- Functions/Exercises/exercise 2.py
@@ -1,7 +1,4 @@
-'''## Exercise 2: Favorite Book :ballot_box_with_check:
-Write a function called favorite_book() that accepts one parameter, title. 
-The function should print a message, such as One of my favorite books is Alice in Wonderland. 
-Call the function, making sure to include a book title as an argument in the function call.'''
+# Exercise 2: Favorite Book
 
 #defining function that takes one parameter
 def favorite_book(title):
--- Chapter 7- Functions/Exercises/exercise 3.py
+++ Chapter 7- Functions/Exercises/exercise 3.py
@@ -1,9 +1,4 @@
-'''# Exercise 3: T-Shirt  :ballot_box_with_check:
-Write a function called make_shirt() that accepts a size and the text of a message that should be printed 
-on the shirt. 
-The function should print a sentence summarizing the size of the shirt and the message printed on it. 
-Call the function once using positional arguments to make a shirt. 
-Call the function a second time using keyword arguments.'''
+#Exercise 3: T-Shirt
 
 #defining a function with two parameters
 def make_shirt(size, message):
